f81989734c1b7170fff91adff0a7d1bac33b6eb4
Wu Zhangjin <wuzhangjin@gmail.com>
2015-05-03 22:49:33 +0800

    Use Gitbook support
    TODO: Fix up the failures in the output.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

---
+++ tools/convert2gitbook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# convert2gitbook.sh -- convert the old markdown book support to gitbook
+#
+
+LANG=zh
+
+# Generate SUMMARY.md
+
+find $LANG/ -name "*.markdown*" | xargs -i grep -Hr "^# " {} \
+        | grep -v "define" | sort -t / -g -k 3 \
+        | sed -e "s/\(.*\):# \(.*\)/* [\2](\1)/g" > SUMMARY.md
+
+# Convert pic/cover.png to cover.jpg
+# Note: config/basic.yml doesn't use pic/cover.png currently.
+
+convert pic/cover.png cover.jpg
